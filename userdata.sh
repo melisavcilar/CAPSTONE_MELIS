e6c2d90b922a0ff2d9fdc421029bfa2d1f72c568
Melisavcilar <109428813+melisavcilar@users.noreply.github.com>
2022-10-18 13:32:39 -0700

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -7,7 +7,7 @@ TOKEN="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"
 git clone https://$TOKEN@github.com/melisavcilar/CAPSTONE_MELIS.git
 cd /home/ubuntu/CAPSTONE_MELIS
 apt install python3-pip -y
-apt-get install python3.7-dev libmysqlclient-dev -y
+apt-get install python3.7-dev default-libmysqlclient-dev -y
 pip3 install -r requirements.txt
 cd /home/ubuntu/CAPSTONE_MELIS/src
 python3 manage.py collectstatic --noinput
