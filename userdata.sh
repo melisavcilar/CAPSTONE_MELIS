b1bf16da18e7c376b064c020c441ea25d54c0787
melisavcilar <melisavcilar@gmail.com>
2023-01-08 16:57:02 -0800

    files are edited

--- userdata.sh
+++ userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="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"
-git clone https://$TOKEN@github.com/melisavcilar/CAPSTONE_MELIS.git
-cd /home/ubuntu/CAPSTONE_MELIS
+TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+git clone https://$TOKEN@<YOUR GITHUB REPO URL>
+cd /home/ubuntu/<YOUR GITHUB REPO NAME>
 apt install python3-pip -y
-apt-get install python3.7-dev default-libmysqlclient-dev -y
+apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/CAPSTONE_MELIS/src
+cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
